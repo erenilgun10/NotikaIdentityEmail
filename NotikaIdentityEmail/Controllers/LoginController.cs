412ac7e6e559733f5ccd055fa4c296358aa55056
Eren İlgün <eren.ilgn.10@hotmail.com>
2025-11-11 16:37:37 +0200

    .

--- NotikaIdentityEmail/Controllers/LoginController.cs
+++ NotikaIdentityEmail/Controllers/LoginController.cs
@@ -1,10 +1,12 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NotikaIdentityEmail.Entities;
 using NotikaIdentityEmail.Models;
 
 namespace NotikaIdentityEmail.Controllers
 {
-    public class LoginController : Controller
+    public class LoginController(SignInManager<AppUser> signInManager) : Controller
     {
         [HttpGet]
         public IActionResult UserLogin()
@@ -15,7 +17,11 @@ namespace NotikaIdentityEmail.Controllers
         [HttpPost]
         public async Task<IActionResult> UserLogin(UserLoginViewModel model)
         {
-            return View();
+            var result = await signInManager.PasswordSignInAsync(model.Username, model.Password, true, true);
+            if (result.Succeeded) return RedirectToAction("Profile", "MyProfile");
+            else ModelState.AddModelError("", "Kullanıcı Adı yada Şifre Yanlış");
+            return View(model);
+
         }
 
 
