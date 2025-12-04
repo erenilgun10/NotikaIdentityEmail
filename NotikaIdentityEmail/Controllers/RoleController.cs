2093731c4476373f0eb3b85a8cc02c005dd0e10a
Eren İlgün <eren.ilgn.10@gmail.com>
2025-12-04 16:42:54 +0200

    UPDATEs

---
+++ NotikaIdentityEmail/Controllers/RoleController.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using NotikaIdentityEmail.Context;
+using NotikaIdentityEmail.Entities;
+using NotikaIdentityEmail.Models;
+using NotikaIdentityEmail.Services;
+using System.Threading.Tasks;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class RoleController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager) : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            IdentityRole identityRole = new()
+            {
+                Name = model.RoleName
+            };
+            await roleManager.CreateAsync(identityRole);
+
+            return RedirectToAction("RoleList");
+        }
+
+
+
+
+
+
+
+
+    }
+}
