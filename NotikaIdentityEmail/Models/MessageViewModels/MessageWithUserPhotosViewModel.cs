e6dc837c3df724242415904f472ec754b0f41149
Eren İlgün <eren.ilgn.10@hotmail.com>
2025-12-09 13:41:11 +0200

    Güncellemeler yapıldı

---
+++ NotikaIdentityEmail/Models/MessageViewModels/MessageWithUserPhotosViewModel.cs
@@ -0,0 +1,12 @@
+﻿using NotikaIdentityEmail.Entities;
+
+namespace NotikaIdentityEmail.Models.MessageViewModels
+{
+    public class MessageWithUserPhotosViewModel
+    {
+        public string? FullName { get; set; }
+        public string? ImageUrl { get; set; }
+        public Message? Message { get; set; }   
+
+    }
+}
