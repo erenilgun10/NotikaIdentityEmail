850014f8ad717af789b181ed2303acd43974b53a
Eren İlgün <eren.ilgn.10@hotmail.com>
2025-12-10 21:37:35 +0200

    Last version update

---
+++ NotikaIdentityEmail/Controllers/CommentController.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MimeKit;
+using NotikaIdentityEmail.Context;
+using NotikaIdentityEmail.Entities;
+using NotikaIdentityEmail.Models.CommentViewModels;
+using NotikaIdentityEmail.Models.IdentityModels;
+using NotikaIdentityEmail.Services;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class CommentController(EmailContext context) : Controller
+    {
+
+        [HttpGet]
+        public IActionResult UserComment()
+        {
+
+            var comments = context.Comments.Include(x=>x.AppUser).ToList();
+
+            return View(comments);
+        }
+
+        [HttpGet]
+        public IActionResult UserCommentList()
+        {
+            var userList = context.Comments.Include(x => x.AppUser).ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CommentViewModel model)
+        {
+            
+            if (ModelState.IsValid && model.Comment is not null)
+            {
+                
+                context.Comments.Add(model.Comment);
+                await context.SaveChangesAsync();
+                return RedirectToAction("UserComment");
+            }
+            return View(model);
+
+
+        }
+
+
+
+    }
+}
---
+++ NotikaIdentityEmail/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NotikaIdentityEmail.Entities
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public required string AppUserId { get; set; }
+        public string? CommentDetail { get; set; }
+        public DateTime CommentDate { get; set; }
+        public string? CommentStatus { get; set; }
+
+        public AppUser AppUser { get; set; } = new();
+
+    }
+}
